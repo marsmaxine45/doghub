eedb4f9ff28f9c6365bc3d8f702369cb50b5fe4f
marsmaxine45 <solomonbayonde@gmail.com>
2019-11-22 19:15:24 +0300

    BayondeFabrics

---
+++ Js/app.js
@@ -0,0 +1,27 @@
+var anyName = document.getElementById("clickMe");
+var moreText="gfgfghdfhgfhgfj";
+anyName.onclick=(function(){
+    alert("i've been clicked");
+    let kentheninja= document.getElementById("aboutProject");
+kentheninja.innerHTML=moreText;
+});
+
+
+ 
+
+// var alertMessage = "you have reached solomon";
+
+// var tapButton = document.getElementById("clickMe");
+// function execute(){
+//     alert(alertmessage)
+//     console.log("Button has been clicked")
+//     anyName.onclick=()=>{
+      
+//     }
+// }
+// tapbutton.onclick = ( function(){
+//     execute();
+// });
+// let names = [
+//     "solomon", "mercy", "chairs"
+// ]
